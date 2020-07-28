6494064066494213e9640e2a51d5e45a73bc7f91
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2020-07-28 17:05:20 -0300

    fix: funcao assíncrona retornando promise
    Agora ao utilizar a função, é possível tratar o resultado como uma promise

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const axios = require('axios')
 const IFRNSC_REPOSITORIOS_URL = 'https://api.github.com/orgs/IFRN-SC/repos'
 
 module.exports = {
-    getIfrnScRepositorios() {
-        axios.get(IFRNSC_REPOSITORIOS_URL)
-            .then(res => {
-                const repositorios = res.data
-
-                return repositorios.map(repo => { return repo.name })
-            })
+    /**
+     * Função assíncrona que retorna uma array com todos os repositórios do IFRN-SC no Github
+     * 
+     * @author isaacmsl
+     */ 
+    async getIfrnScRepositorios() {
+        const response = await axios.get(IFRNSC_REPOSITORIOS_URL)
+        const repositorios = response.data
+        
+        return repositorios.map(repo => { return repo.name })
     }
 }
